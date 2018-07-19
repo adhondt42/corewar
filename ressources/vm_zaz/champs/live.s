550f8ea59e64643a2724efd575b6a6539bf4523f
Axel DHONDT <adhondt@e3r10p10.42.fr>
2018-07-20 01:44:50 +0200

    VM can load multiples binary files

--- ressources/vm_zaz/champs/live.s
+++
@@ -1,7 +0,0 @@
-.name "Rhodiaiaaa"
-.comment "just a basic living prog"
-		
-live %-10000
-#start:	sti r1,%:core,%1
-#core:	live
-#loop:	zjmp %:start
--- ressources/vm_zaz/players/proto.s
+++
@@ -1,9 +0,0 @@
-.name "proto"
-.comment "proto"
-
-cary:		ld %0, r2
-			st r1, 6
-deb:		live %:deb
-			zjmp %:deb
-
-
